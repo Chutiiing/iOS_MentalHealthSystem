02444d7366673657e34f6ce6da6fa9612d44ed80
Chutiiing <996071760@qq.com>
2022-04-28 14:00:27 +0800

    Initial Commit

---
+++ MentalSystem/MentalSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  MentalSystemApp.swift
+//  MentalSystem
+//
+//  Created by 林楚婷 on 2022/4/28.
+//
+
+import SwiftUI
+
+@main
+struct MentalSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
